bec5a322333a9c815a7f1217c8500c2385541770
sritar99 <sadhutarun00@gmail.com>
2022-01-31 13:13:01 +0530

    Added Filtering the Year (included useState)

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,21 +1,37 @@
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import Card from "../UI/Card";
+import ExpenseFilter from "./ExpenseFilter";
+import React,{ useState } from 'react';
+
+
 function Expenses(props) {
+
+  const [filteredYear,setFilteredYear]=useState('2022');
+
+
+  function filterChangeHandler(selectedYear){
+    console.log("selected Year : "+selectedYear);
+    setFilteredYear(selectedYear);
+  }
+
   return (
-    <Card className="expenses">
-      <ExpenseItem
-        title={props.expenses[0].title}
-        amount={props.expenses[0].amount}
-        date={props.expenses[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={props.expenses[1].title}
-        amount={props.expenses[1].amount}
-        date={props.expenses[1].date}
-      ></ExpenseItem>
-    </Card>
+    <div>
+      <Card className="expenses">
+        <ExpenseFilter defaultYear={filteredYear} onFilterYearChange={filterChangeHandler}></ExpenseFilter>
+        <ExpenseItem
+          title={props.expenses[0].title}
+          amount={props.expenses[0].amount}
+          date={props.expenses[0].date}
+        ></ExpenseItem>
+        <ExpenseItem
+          title={props.expenses[1].title}
+          amount={props.expenses[1].amount}
+          date={props.expenses[1].date}
+        ></ExpenseItem>
+      </Card>
+    </div>
   );
 }
 
-export default Expenses
+export default Expenses;
